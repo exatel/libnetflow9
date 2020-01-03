952414986d707828c357f007de5178ef4ae36156
Michał Krzywkowski <michal.krzywkowski@exatel.pl>
2020-01-03 12:17:18 +0100

    Use a "cursor" for safely navigating and retrieving values from a byte buffer

--- src/parse.cpp
+++ src/parse.cpp
@@ -1,10 +1,43 @@
 #include "parse.h"
+#include <cassert>
 #include <cstring>
 
 struct flowset_header
 {
     uint16_t flowset_id;
     uint16_t length;
+};
+
+// Cursor for a byte buffer.  Allows safely retrieving values from the
+// underlying buffer.
+struct buffer
+{
+    const uint8_t* const buf;
+    const size_t len;
+    const uint8_t* ptr;
+
+    // How many bytes do we have left?
+    size_t remaining() const
+    {
+        assert(ptr >= buf && size_t(ptr - buf) <= len);
+        return len - (ptr - buf);
+    }
+
+    bool get(void* dst, size_t size)
+    {
+        if (remaining() < size)
+            return false;
+
+        memcpy(dst, ptr, size);
+        advance(size);
+        return true;
+    }
+
+    void advance(size_t n)
+    {
+        assert(n <= remaining());
+        ptr += n;
+    }
 };
 
 static nf9_flowset_type get_flowset_type(const flowset_header* header)
@@ -18,14 +51,10 @@ static nf9_flowset_type get_flowset_type(const flowset_header* header)
     return NF9_FLOWSET_TEMPLATE;
 }
 
-static bool parse_header(const uint8_t** buf, size_t* len, netflow_header* hdr)
+static bool parse_header(buffer& buf, netflow_header* hdr)
 {
-    if (*len < sizeof(*hdr))
+    if (!buf.get(hdr, sizeof(*hdr)))
         return false;
-    memcpy(hdr, *buf, sizeof(*hdr));
-
-    *buf += sizeof(*hdr);
-    *len -= sizeof(*hdr);
 
     if (ntohs(hdr->version) != 9)
         return false;
@@ -33,20 +62,16 @@ static bool parse_header(const uint8_t** buf, size_t* len, netflow_header* hdr)
     return true;
 }
 
-static bool parse_data_template(const uint8_t** buf, size_t* len,
-                                data_template& result)
+static bool parse_data_template(buffer& buf, data_template& result)
 {
-    if (*len < 2 * sizeof(uint16_t))
-        return false;
-
     uint16_t type;
     uint16_t length;
 
-    memcpy(&type, *buf, sizeof(type));
-    *buf += sizeof(type);
-    memcpy(&length, *buf, sizeof(length));
-    *buf += sizeof(length);
-    *len -= 2 * sizeof(uint16_t);
+    if (!buf.get(&type, sizeof(type)))
+        return false;
+
+    if (!buf.get(&length, sizeof(length)))
+        return false;
 
     type = ntohs(type);
     length = ntohs(length);
@@ -57,21 +82,16 @@ static bool parse_data_template(const uint8_t** buf, size_t* len,
     return true;
 }
 
-static bool parse_data_template_flowset(const uint8_t* buf, size_t len,
-                                        nf9_state* state,
+static bool parse_data_template_flowset(buffer& buf, nf9_state* state,
                                         nf9_parse_result* result)
 {
-    if (len < 2 * sizeof(uint16_t))
-        return false;
-
     uint16_t template_id;
     uint16_t field_count;
 
-    memcpy(&template_id, buf, sizeof(template_id));
-    buf += sizeof(template_id);
-    memcpy(&field_count, buf, sizeof(field_count));
-    buf += sizeof(field_count);
-    len -= 2 * sizeof(uint16_t);
+    if (!buf.get(&template_id, sizeof(template_id)))
+        return false;
+    if (!buf.get(&field_count, sizeof(field_count)))
+        return false;
 
     template_id = ntohs(template_id);
     field_count = ntohs(field_count);
@@ -80,8 +100,8 @@ static bool parse_data_template_flowset(const uint8_t* buf, size_t len,
     flowset& f = result->flowsets.back();
     data_template& tmpl = f.dtemplate;
 
-    while (field_count-- > 0 && len > 0) {
-        if (!parse_data_template(&buf, &len, tmpl))
+    while (field_count-- > 0 && buf.remaining() > 0) {
+        if (!parse_data_template(buf, tmpl))
             return false;
     }
 
@@ -90,18 +110,15 @@ static bool parse_data_template_flowset(const uint8_t* buf, size_t len,
     return true;
 }
 
-static bool parse_flow(const uint8_t** buf, size_t* len, data_template& tmpl,
-                       flowset& result)
+static bool parse_flow(buffer& buf, data_template& tmpl, flowset& result)
 {
     if (tmpl.fields.empty()) {
-        *buf += *len;
-        *len = 0;
+        buf.advance(buf.remaining());
         return true;
     }
 
-    if (tmpl.total_length > *len) {
-        *buf += *len;
-        *len = 0;
+    if (tmpl.total_length > buf.remaining()) {
+        buf.advance(buf.remaining());
         return true;
     }
 
@@ -112,17 +129,15 @@ static bool parse_flow(const uint8_t** buf, size_t* len, data_template& tmpl,
         int type = tf.first;
         size_t field_length = tf.second;
 
-        if (field_length > *len)
+        if (field_length > buf.remaining())
             return false;
 
         if (field_length == 0)
             break;
 
         std::vector<uint8_t> field_value;
-        field_value.assign(*buf, *buf + field_length);
-
-        *buf += field_length;
-        *len -= field_length;
+        field_value.assign(field_length, 0);
+        buf.get(field_value.data(), field_length);
 
         f[type] = field_value;
     }
@@ -130,39 +145,35 @@ static bool parse_flow(const uint8_t** buf, size_t* len, data_template& tmpl,
     return true;
 }
 
-static bool parse_data_flowset(const uint8_t* buf, size_t len, nf9_state* state,
+static bool parse_data_flowset(buffer& buf, nf9_state* state,
                                uint16_t template_id, nf9_parse_result* result)
 {
     result->flowsets.push_back(flowset{NF9_FLOWSET_DATA});
 
     if (state->templates.count(template_id) == 0) {
         state->stats.missing_template_errors++;
+        buf.advance(buf.remaining());
         return true;
     }
 
     flowset& fset = result->flowsets.back();
     data_template& tmpl = state->templates[template_id];
 
-    while (len > 0) {
-        if (!parse_flow(&buf, &len, tmpl, fset))
+    while (buf.remaining() > 0) {
+        if (!parse_flow(buf, tmpl, fset))
             return false;
     }
 
     return true;
 }
 
-static bool parse_flowset(const uint8_t** buf, size_t* len,
-                          nf9_parse_result* result, nf9_state* state)
+static bool parse_flowset(buffer& buf, nf9_parse_result* result,
+                          nf9_state* state)
 {
     flowset_header header;
 
-    if (*len < sizeof(header))
+    if (!buf.get(&header, sizeof(header)))
         return false;
-
-    memcpy(&header, *buf, sizeof(header));
-
-    *buf += sizeof(header);
-    *len -= sizeof(header);
 
     size_t flowset_length = ntohs(header.length);
 
@@ -173,13 +184,16 @@ static bool parse_flowset(const uint8_t** buf, size_t* len,
         return false;
 
     flowset_length -= sizeof(header);
-    if (flowset_length > *len)
+    if (flowset_length > buf.remaining())
         return false;
+
+    buffer tmpbuf{buf.ptr, flowset_length, buf.ptr};
+    buf.advance(flowset_length);
 
     switch (get_flowset_type(&header)) {
         case NF9_FLOWSET_TEMPLATE: {
             state->stats.templates++;
-            if (!parse_data_template_flowset(*buf, *len, state, result))
+            if (!parse_data_template_flowset(tmpbuf, state, result))
                 return false;
             break;
         }
@@ -189,7 +203,7 @@ static bool parse_flowset(const uint8_t** buf, size_t* len,
             break;
         case NF9_FLOWSET_DATA:
             state->stats.records++;
-            if (!parse_data_flowset(*buf, *len, state, ntohs(header.flowset_id),
+            if (!parse_data_flowset(tmpbuf, state, ntohs(header.flowset_id),
                                     result))
                 return false;
             break;
@@ -197,23 +211,22 @@ static bool parse_flowset(const uint8_t** buf, size_t* len,
             state->stats.malformed_packets++;
             return false;
     }
-    *buf += flowset_length;
-    *len -= flowset_length;
 
     return true;
 }
 
-bool parse(const uint8_t* buf, size_t len, nf9_state* state,
+bool parse(const uint8_t* data, size_t len, nf9_state* state,
            nf9_parse_result* result)
 {
+    buffer buf{data, len, data};
     netflow_header header;
 
-    if (!parse_header(&buf, &len, &header))
+    if (!parse_header(buf, &header))
         return false;
 
     size_t num_flowsets = ntohs(header.count);
-    for (size_t i = 0; i < num_flowsets && len > 0; i++) {
-        if (!parse_flowset(&buf, &len, result, state))
+    for (size_t i = 0; i < num_flowsets && buf.remaining() > 0; i++) {
+        if (!parse_flowset(buf, result, state))
             return false;
     }
 
